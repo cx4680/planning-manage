b61eb1190400381a92bab60d352b36a8992642c2
刘蓉 <liurong@cestc.cn>
2023-12-12 10:50:46 +0800

    update:必选错误码

--- internal/entity/cloud_product_planning.go
+++ internal/entity/cloud_product_planning.go
@@ -9,9 +9,9 @@ type CloudProductPlanning struct {
 	PlanId      int64     `gorm:"column:plan_id" json:"planId"`           // 方案id
 	ProductId   int64     `gorm:"column:product_id" json:"productId"`     // 产品id
 	SellSpec    string    `gorm:"column:sell_spec" json:"sellSpec"`       // 售卖规格
-	ServiceYear int       `gorm:"column:service_year" json:"serviceYear"` // 是否必选，0：否，1：是
+	ServiceYear int       `gorm:"column:service_year" json:"serviceYear"` // 维保年限
 	CreateTime  time.Time `gorm:"column:create_time" json:"createTime"`   // 创建时间
-	UpdateTime  time.Time `gorm:"column:update_time" json:"updateTime"`   //更新时间
+	UpdateTime  time.Time `gorm:"column:update_time" json:"updateTime"`   // 更新时间
 }
 
 func (entity *CloudProductPlanning) TableName() string {
